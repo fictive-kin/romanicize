16b9784a910f2cfa9ce365abc86ed35c3d9c1d8c
Jared McKnight <jmcknight@archertech.ca>
2025-06-24 11:40:34 -0400

    bump version and update classfiers/compatible versions

--- setup.py
+++ setup.py
@@ -1,25 +1,29 @@
-
 import os
 import setuptools
 
+
 def readme():
     path = os.path.dirname(__file__)
-    with open(os.path.join(path, 'README.rst')) as f:
+    with open(os.path.join(path, "README.rst")) as f:
         return f.read()
 
-name = 'romanicize'
-description = 'Roman Numeral Conversion Utilities'
-version = '0.1.1'
-author = 'Fictive Kin LLC'
-email = 'hello@fictivekin.com'
+
+name = "romanicize"
+description = "Roman Numeral Conversion Utilities"
+version = "0.1.2"
+author = "Fictive Kin LLC"
+email = "hello@fictivekin.com"
 classifiers = [
-    'Development Status :: 3 - Alpha',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Topic :: Software Development',
+    "Development Status :: 4 - Beta",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3.13",
+    "Topic :: Software Development",
 ]
 
 if __name__ == "__main__":
@@ -29,17 +33,17 @@ if __name__ == "__main__":
         description=description,
         long_description=readme(),
         classifiers=classifiers,
-        url='https://github.com/fictivekin/romanicize',
+        url="https://github.com/fictive-kin/romanicize",
         author=author,
         author_email=email,
         maintainer=author,
         maintainer_email=email,
-        license='MIT',
+        license="MIT",
         python_requires=">=3.6",
         packages=[
-            'romanicize',
+            "romanicize",
         ],
         scripts=[
-            'bin/romanicize',
+            "bin/romanicize",
         ],
     )
